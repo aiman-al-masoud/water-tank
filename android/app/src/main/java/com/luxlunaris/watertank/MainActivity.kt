af5124df44a22b44c0afe2ad81c7eca935dccb50
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-06-09 12:26:06 +0200

    some droid stuff

--- android/app/src/main/java/com/luxlunaris/watertank/MainActivity.kt
+++ android/app/src/main/java/com/luxlunaris/watertank/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.luxlunaris.watertank
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ProgressDialog
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.luxlunaris.watertank.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dialog = ProgressDialog(this)
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+        dialog.setTitle("Loading")
+        dialog.setMessage("Loading. Please wait...")
+        dialog.isIndeterminate = true
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+//        dialog.dismiss()
+
     }
 }
